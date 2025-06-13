6089956e3b03ba84a06667caf296ff054f8771b4
genacanas <2410677@ucc.edu.ar>
2025-06-13 16:43:52 -0300

    Validacion de inscripcion, ahota te deja inscribirte a la misma actividad distintos dias

--- backend/clients/inscripciones/inscripcion_client.go
+++ backend/clients/inscripciones/inscripcion_client.go
@@ -4,7 +4,7 @@ import (
 	"arqsoft_proyecto/model"
 	"gorm.io/gorm"
 	log "github.com/sirupsen/logrus"
-	"fmt"
+	
 	e "arqsoft_proyecto/utils/errors"
 )
 
@@ -12,10 +12,8 @@ var Db *gorm.DB
 
 func InscripcionActividad(inscripcion model.Inscripcion) (model.Inscripcion, error) {
 	var verificadorInscripcion model.Inscripcion
-	txn := Db.Where("usuario_id = ? AND actividad_id = ?", inscripcion.UsuarioId, inscripcion.ActividadId).First(&verificadorInscripcion)
-	if txn.Error != nil {
-		return model.Inscripcion{}, fmt.Errorf("error getting inscripcion: %w", txn.Error)
-	}
+	Db.Where("usuario_id = ? AND actividad_id = ? AND horario_id = ?", inscripcion.UsuarioId, inscripcion.ActividadId, inscripcion.HorarioId).First(&verificadorInscripcion)
+	
 	if verificadorInscripcion.Id != 0{
 		return inscripcion, e.NewBadRequestApiError("ya esta inscripto a la actividad")
 	}
